5a006ab2d5993198a2550a97019159d6e1911388
Matthew <matthewlee031205@gmail.com>
2020-09-13 07:53:21 -0400

    Update removecourse.js

--- commands/removecourse.js
+++ commands/removecourse.js
@@ -1,10 +1,14 @@
 const Discord = require("discord.js");
 const config = require("../botconfig.json");
 const utils = require("../util/utils");
+const utils = require("../util/utils");
+
 
 module.exports.run = async (bot, message, args) => {
     var re = new RegExp("[A-Z]{3}[A-E1-4][OMUCDPELX][M0-9]");
     var list = require('../course_list.json');
+    var rank  = message.member.highestRole.CalculatedPosition;
+    var botrank = bot.highestRole.CalculatedPosition;
     let roleown;
     for(role in message.member.roles){
         if (role == message.content){
@@ -14,11 +18,16 @@ module.exports.run = async (bot, message, args) => {
     if (re.test(message.content)){
         for (entry in list){
             if(message.content == entry && roleown == True){
-                message.member.roles.remove(message.content)
-                message.channel.send(`deleted ${role.name} from ${message.member.name}`)
+                if(rank < botrank){
+                    message.member.roles.remove(message.content)
+                    utils.simpleMessage(`deleted ${role.name} from ${message.member.name}.`,message, config.errorColor, 4 * config.tempMsgTime)
+                }
+            }
+            else if (rank > botrank){
+                utils.simpleMessage(`The bot does not have sufficient permission to edit the role you request.`,message, config.errorColor, 4 * config.tempMsgTime)
             }
             else{
-                message.channel.send(`You do not have this role.`)
+                utils.simpleMessage(`You do not have this role.`,message, config.errorColor, 4 * config.tempMsgTime)
             }
         }
     }
