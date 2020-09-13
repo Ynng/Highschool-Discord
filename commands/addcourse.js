1049578a6766cba3efa1fd82e96491d3c7b43e77
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-09-12 22:37:56 -0400

    addcourse now parses course list properly

--- commands/addcourse.js
+++ commands/addcourse.js
@@ -1,25 +1,37 @@
 const Discord = require("discord.js");
 const config = require("../botconfig.json");
 const utils = require("../util/utils");
+const list = require('../course_list.json');
 
 
 module.exports.run = async (bot, message, args) => {
-    var re = new RegExp("[A-Z]{3}[A-E1-4][OMUCDPELX][M0-9]");
-    var list = require('../course_list.json');
-    var noMatch = true;
-    if(re.test(message.content)) {
-        for(entry in list){
-            if(message.content==entry){
-                message.channel.send(`works format, ${message.content}`);
-                noMatch = false;
-            }
-        }
-    } else {
-        return message.channel.send(`${message.content} invalid`);
+    var re = new RegExp(/[A-Z]{3}[A-E1-4][OMUCDPELX][M0-9]/g);
+    utils.safeDeleteMessage(message, 1000);
+
+
+
+    var potentialCourses = message.content.match(re);
+
+    
+    if(! potentialCourses) {
+        return utils.simpleMessage(":thinking: The course code is not valid, contact an admin for help!", message, config.errorColor, config.tempMsgTime);
     }
 
+    noMatch = true;
+    var courses = [];
+    for(entry in list){
+        potentialCourses.forEach(course => {
+            if(course==entry){
+                courses.push(course);
+                noMatch = false;
+            }
+        })
+    }
+
+    console.log(courses);
+
     if(noMatch){
-        message.channel.send("course doesn't exist");
+        return utils.simpleMessage(":thinking: The course code is not valid, contact an admin for help!", message, config.errorColor, config.tempMsgTime);
     }
 };
 
@@ -27,6 +39,6 @@ module.exports.help = {
     name: "Add Course",
     args: "Any text containing course codes",
     description: "Add a new course to your user!",
-    example: "&addcourse ENG1D0",
+    example: "&addcourse ENG1D0 COP4X0-01 MDM4U1-01 : Mathematics of Data",
     aliases: ["addcourse"]
 };
