2179da743adfd71b3d22eba94aa33b6150790f6f
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-09-13 17:25:13 -0400

    improved serverinfo

--- commands/serverInfo.js
+++ commands/serverInfo.js
@@ -13,13 +13,12 @@ module.exports.run = async (bot, message) => {
           .setThumbnail(serverIcon)
           .setTitle("Server Information!")
           .addField("You joined on", message.member.joinedAt, true)
+          .addField("Region", message.guild.region, true)
+          .addField("Server Owner", message.guild.owner.user.tag, true)
           .addField("Total Members", `**${message.guild.memberCount}**`, true)
-          .addField('\u200b', '\u200b')
           .addField('Humans', `**${message.guild.members.cache.filter(member => !member.user.bot).size}**`, true)
           .addField('Bots', `**${message.guild.members.cache.filter(member => member.user.bot).size}**`, true)
-          .addField('Member Status', `**${message.guild.members.cache.filter(o => o.presence.status === 'online').size}** Online\n**${message.guild.members.cache.filter(i => i.presence.status === 'idle').size}** Idle/Away\n**${message.guild.members.cache.filter(dnd => dnd.presence.status === 'dnd').size}** Do Not Disturb\n**${message.guild.members.cache.filter(off => off.presence.status === 'offline').size}** Offline/Invisible\n**${message.guild.members.cache.filter(s => s.presence.status === 'streaming').size}** Streaming`)
-          .addField("Server Owner", message.guild.owner.user.tag, true)
-          .addField("Region", message.guild.region)
+          .addField('Member Status', `**${message.guild.members.cache.filter(o => o.presence.status === 'online').size}** Online\n**${message.guild.members.cache.filter(i => i.presence.status === 'idle').size}** Idle/Away\n**${message.guild.members.cache.filter(dnd => dnd.presence.status === 'dnd').size}** Do Not Disturb\n**${message.guild.members.cache.filter(off => off.presence.status === 'offline').size}** Offline/Invisible\n**${message.guild.members.cache.filter(s => s.presence.status === 'streaming').size}** Streaming`, true)
           .setColor(config.embedColor);
   utils.embedAddStamp(message, embed, message.author);
   message.channel.send(embed);
