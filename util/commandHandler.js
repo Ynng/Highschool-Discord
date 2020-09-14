40fe29fbed183feb2d84e12d6bd9dc38a66bff33
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-09-13 22:04:16 -0400

    improved spam prevention

--- util/commandHandler.js
+++ util/commandHandler.js
@@ -1,7 +1,13 @@
 const config = require("../botconfig.json");
+const utils = require("./utils.js");
 const fs = require('fs');
-const recent = new Set();
-const timeouttime = 3000;
+const recent = {};
+
+async function resetSpamCount(userId) {
+    setTimeout(() => {
+        recent[userId]--;
+    }, config.spamTime);
+}
 
 module.exports = (bot, message) => {
     var prefix = config.prefix;
@@ -14,9 +20,23 @@ module.exports = (bot, message) => {
         } else {
             msg = message.content;
         }
+        if (!(message.author.id in recent))
+            recent[message.author.id] = 0;
+        recent[message.author.id]++;
+        resetSpamCount(message.author.id);
 
-        if (recent.has(message.author.id))
-            return utils.simpleMessage(`:tired_face: Woah there! You're going too fast, slow down!`, message, config.loadingColor, config.tempMsgTime);
+        if (recent[message.author.id] >= config.spamMessageCount) {
+            if (message.channel.name === config.welcomeChannel)
+                utils.safeDeleteMessage(message);
+            if (recent[message.author.id] == config.spamMessageCount)
+                utils.simpleMessage(`:tired_face: Woah there! You're going too fast, wait ${config.spamTime/1000} seconds  before trying that again!`, message, config.warningColor, config.tempMsgTime);
+            fs.appendFile('log.txt', `@${message.author.username} just spammed "${recent[message.author.id]}"\n\n`, function (err) {
+                if (err) throw err;
+                console.log('Saved!');
+            });
+
+            return;
+        }
 
         if (message.channel.name === config.welcomeChannel) {
             var commmandfile = bot.commands.get(bot.aliases.get("addcourse"));
@@ -38,7 +58,5 @@ module.exports = (bot, message) => {
         if (commmandfile) {
             commmandfile.run(bot, message, args);
         }
-        recent.add(message.author.id);
-        setTimeout(recent.delete(message.author.id), timeouttime)
     }
 };
