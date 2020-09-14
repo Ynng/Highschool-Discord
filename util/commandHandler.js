57ba54fff4ed5d0523aa707736b8b45c22cea1b9
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-09-13 21:08:25 -0400

    formatted commandhandler

--- util/commandHandler.js
+++ util/commandHandler.js
@@ -5,30 +5,30 @@ const timeouttime = 3000;
 
 module.exports = (bot, message) => {
     var prefix = config.prefix;
-    if (message.content.startsWith(prefix) || message.content.startsWith(`<@!${bot.user.id}>`) || message.channel.type == "dm"||message.channel.name === config.welcomeChannel) {
+    if (message.content.startsWith(prefix) || message.content.startsWith(`<@!${bot.user.id}>`) || message.channel.type == "dm" || message.channel.name === config.welcomeChannel) {
         var msg;
         if (message.content.startsWith(prefix)) {
             msg = message.content.substring(prefix.length).trim();
-        } else if(message.content.startsWith(`<@!${bot.user.id}>`)){
-            msg = message.content.substring(bot.user.id.length+4).trim();   
-        }else {
+        } else if (message.content.startsWith(`<@!${bot.user.id}>`)) {
+            msg = message.content.substring(bot.user.id.length + 4).trim();
+        } else {
             msg = message.content;
         }
 
-        if(recent.has(message.author.id))
+        if (recent.has(message.author.id))
             return utils.simpleMessage(`:tired_face: Woah there! You're going too fast, slow down!`, message, config.loadingColor, config.tempMsgTime);
 
-        if(message.channel.name === config.welcomeChannel){
+        if (message.channel.name === config.welcomeChannel) {
             var commmandfile = bot.commands.get(bot.aliases.get("addcourse"));
             fs.appendFile('log.txt', `@${message.author.username} just sent "${msg}"\n\n`, function (err) {
                 if (err) throw err;
                 console.log('Saved!');
             });
-        }else{
+        } else {
             msg = msg.replace(/\s+/g, ' ');
             var args = msg.split(" ");
             var cmd = args.shift().toLowerCase();
-            
+
             fs.appendFile('log.txt', `@${message.author.username} just requested "${cmd}" with args "${args}"\n\n`, function (err) {
                 if (err) throw err;
                 console.log('Saved!');
@@ -39,6 +39,6 @@ module.exports = (bot, message) => {
             commmandfile.run(bot, message, args);
         }
         recent.add(message.author.id);
-        setTimeout(recent.delete(message.author.id),timeouttime)
+        setTimeout(recent.delete(message.author.id), timeouttime)
     }
 };
