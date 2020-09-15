6a1d85094b27aa2cb26d79bd50982d949ccbf36f
Kevin Huang <44907675+Ynng@users.noreply.github.com>
2020-09-15 18:35:58 -0400

    better ready.js

--- events/ready.js
+++ events/ready.js
@@ -3,14 +3,14 @@ const config = require("../botconfig.json");
 
 module.exports = bot => {
     console.log(`${bot.user.username} is online!`);
-    let statuses = [
-        `over ${bot.guilds.cache.size} server(s)`,
-        `over ${bot.users.cache.size} user(s)`
-    ];
 
     let statusNum = 0;
     // console.log(bot.emojis);
     setInterval(() => {
+        let statuses = [
+            `over ${bot.guilds.cache.size} server(s)`,
+            `over ${bot.users.cache.size} user(s)`
+        ];
         let status = statuses[statusNum];
         bot.user.setActivity(`${status} | "${config.prefix}help"`, { type: "WATCHING" })
             // .then(presence => console.log(`Activity set to "${status}"`))
